8ca55c488e46ae2069866857194d603ca0eaa102
metaboulie <eugeneheiner14@gmail.com>
2025-03-26 12:29:19 +0800

    Add cli parsing for 2021/day01

--- 2021/day01/main.nim
+++ 2021/day01/main.nim
@@ -1,4 +1,5 @@
 import strutils, sequtils
+import std/parseopt
 
 proc numOfIncreased(depths: seq[int]): int =
   zip(depths, depths[1..^1]).countIt(it[0] < it[1])
@@ -6,9 +7,17 @@ proc numOfIncreased(depths: seq[int]): int =
 proc numOfSlidingIncreased(depths: seq[int]): int =
   zip(depths, depths[3..^1]).countIt(it[0] < it[1])
 
-proc main(filePath: string) =
-  let depths = readFile(filePath).strip().splitLines().map(parseInt)
+proc main() =
+  var path: string
+  for kind, key, _ in getopt():
+    case kind
+    of cmdArgument:
+      path = key
+    else:
+      discard
+
+  let depths = readFile(path).strip().splitLines().map(parseInt)
   echo "Answer to part 1: ", depths.numOfIncreased
   echo "Answer to part 2: ", depths.numOfSlidingIncreased
 
-main("./input.txt")
+main()
